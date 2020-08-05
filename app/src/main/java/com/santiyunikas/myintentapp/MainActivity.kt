4e4e43b188a2e0bc02bd8f5876e1ea995f83f4d0
santiyunikas <santiyunikas@gmail.com>
2020-08-05 09:27:00 +0700

    Second Commit
    Add Button "Move with Object" and Button "Move for Result"

--- app/src/main/java/com/santiyunikas/myintentapp/MainActivity.kt
+++ app/src/main/java/com/santiyunikas/myintentapp/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.santiyunikas.myintentapp
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.Serializable
+import kotlin.math.log
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener, Serializable {
+
+    var objectName: String = ""
+    var objectAge: Int = 0
+
+        get() = field
+
+        set(value) {
+        field = value
+        }
 
     companion object{
         const val EXTRA_AGE = "extra_age"
         const val EXTRA_NAME = "extra_name"
+        const val EXTRA_OBJECT = "extra_object"
+        const val REQUEST_CODE = "extra_request_code"
+        const val EXTRA_RESULT = "extra_result"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +45,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnDialANumber: Button = findViewById(R.id.dial_a_number)
         btnDialANumber.setOnClickListener(this)
+
+        val btnMoveActivityWithObject: Button = findViewById(R.id.btn_move_activity_w_object)
+        btnMoveActivityWithObject.setOnClickListener(this)
+
+        val btnMoveActivityForResult: Button = findViewById(R.id.btn_move_activity_for_result)
+        btnMoveActivityForResult.setOnClickListener(this)
+
+        val tvResult: TextView = findViewById(R.id.tv_result)
     }
 
     override fun onClick(v: View?) {
@@ -45,6 +72,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveDialANumberIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: $phoneNumber"))
                 startActivity(moveDialANumberIntent)
             }
+            R.id.btn_move_activity_w_object ->{
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                val itObject: MainActivity = MainActivity()
+                itObject.objectName = "Object Santi"
+                itObject.objectAge = 22
+                moveWithObjectIntent.putExtra(MainActivity.EXTRA_OBJECT, itObject)
+                startActivity(moveWithObjectIntent)
+            }
+            R.id.btn_move_activity_for_result ->{
+                val moveActivityForResult = Intent(this@MainActivity, MoveActivity::class.java)
+                moveActivityForResult.putExtra(MainActivity.REQUEST_CODE, 1)
+                startActivityForResult(moveActivityForResult, 1)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode === 1) {
+            if (resultCode === Activity.RESULT_OK) {
+                val result = data?.getStringExtra(MainActivity.EXTRA_RESULT)
+                tv_result.text = result
+            }
+            if (resultCode === Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
         }
     }
 }
+
+
+
--- app/src/main/java/com/santiyunikas/myintentapp/MoveActivity.kt
+++ app/src/main/java/com/santiyunikas/myintentapp/MoveActivity.kt
@@ -1,29 +1,97 @@
 package com.santiyunikas.myintentapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioButton
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_move.*
 
-class MoveActivity : AppCompatActivity() {
+class MoveActivity : AppCompatActivity(), View.OnClickListener{
 
     companion object{
         const val EXTRA_AGE = "extra_age"
         const val EXTRA_NAME = "extra_name"
+        const val EXTRA_OBJECT = "extra_object"
+        const val REQUEST_CODE = "extra_request_code"
+        const val EXTRA_RESULT = "extra_result"
     }
+
+    var result = "defaultValue"
+    get() = field
+    set(value) {field = value}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move)
+        val tvMove: TextView = findViewById(R.id.tvMove)
 
         val name = intent.getStringExtra(MoveActivity.EXTRA_NAME)
         val age = intent.getIntExtra(MoveActivity.EXTRA_AGE, 0)
 
-        val tvMove: TextView = findViewById(R.id.tvMove)
+        val itObject = intent.getSerializableExtra(MoveActivity.EXTRA_OBJECT) as MainActivity?
 
-        if (name != null && age != null)
+        val requestCode = intent.getIntExtra(MoveActivity.REQUEST_CODE, 0)
+
+        val lineLayInside: LinearLayout = findViewById(R.id.line_lay_inside)
+        lineLayInside.visibility = View.INVISIBLE
+        val radBtn10: RadioButton = findViewById(R.id.radioButton10)
+        val radBtn20: RadioButton = findViewById(R.id.radioButton20)
+        val radBtn30: RadioButton = findViewById(R.id.radioButton30)
+        val btnResult: Button = findViewById(R.id.btn_result)
+        btnResult.setOnClickListener(this)
+
+        if (name != null && age != null){
             tvMove.text = """Name: $name
             Age: $age
         """.trimIndent()
+        }else if (itObject != null){
+            tvMove.text = """Name: ${itObject.objectName}
+                |Age: ${itObject.objectAge}
+            """.trimMargin()
+        }else if (requestCode == 1){
+            lineLayInside.visibility = View.VISIBLE
+        }
     }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                radioButton10.id ->
+                    if (checked) {
+                        this.result = radioButton10.text.toString().trim()
+                    }
+                radioButton20.id ->
+                    if (checked) {
+                        this.result = radioButton20.text.toString().trim()
+                    }
+                radioButton30.id ->{
+                    if (checked) {
+                        this.result = radioButton30.text.toString().trim()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_result ->{
+                val resultIntent = Intent(this@MoveActivity, MainActivity::class.java)
+                resultIntent.putExtra(MoveActivity.EXTRA_RESULT, this.result)
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+
 
 }
